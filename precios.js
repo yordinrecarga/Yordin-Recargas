b35531151de0786b7c7dce48c4d79f81b5d9e498
yordinrecarga <yordinrecarga@gmail.com>
2025-09-25 08:32:09 -0400

    Actualizar precios.js

--- precios.js
+++ precios.js
@@ -30,7 +30,7 @@ const freefirePackagesUSD = [
     { id: 3, name: '520+52 Diamantes', priceUSD: 3.90 },
     { id: 4, name: '1050+105 Diamantes', priceUSD: 7.80 },
     { id: 5, name: '2180+218 Diamantes', priceUSD: 15.34 },
-    { id: 6, name: '5600+560 Diamantes', priceUSD: 39.68 },
+    { id: 6, name: '5600+560 Diamantes', priceUSD: 34.68 },
     { id: 7, name: 'Tarjeta Semanal', priceUSD: 2.44 },
     { id: 8, name: 'Tarjeta Mensual', priceUSD: 9.54 },
     { id: 9, name: 'Tarjeta Básica', priceUSD: 0.78 },
