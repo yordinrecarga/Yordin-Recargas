58e1c4f7fa4aa8eeaea5981eea5d355252d52f5a
yordinrecarga <yordinrecarga@gmail.com>
2025-11-26 17:48:01 -0400

    Update precios.js

--- precios.js
+++ precios.js
@@ -25,7 +25,7 @@ const pubgmobilePackagesUSD = [
 
 // Precios base en USD para Free Fire - NO CAMBIAR ESTOS VALORES
 const freefirePackagesUSD = [
-    { id: 1, name: '100+10 Diamantes', priceUSD: 1.30 },
+    { id: 1, name: '100+10 Diamantes', priceUSD: 1.20 },
     { id: 2, name: '310+31 Diamantes', priceUSD: 3.45 },
     { id: 3, name: '520+52 Diamantes', priceUSD: 5.54 },
     { id: 4, name: '1050+105 Diamantes', priceUSD: 10.70 },
