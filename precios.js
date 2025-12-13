42a965a74bc368009843e89c5270c7df96a9cf62
yordinrecarga <yordinrecarga@gmail.com>
2025-12-13 10:05:15 -0400

    Actualizar precios.js

--- precios.js
+++ precios.js
@@ -7,7 +7,7 @@ export const TASA_DOLAR_VES = 298.00; // Tasa de cambio de USD a Bolívares
 // Precios base en USD para Roblox - NO CAMBIAR ESTOS VALORES
 const robloxPackagesUSD = [
     { id: 1, name: '80 Robux', priceUSD: 2.85 },
-    { id: 1, name: '500 Robux', priceUSD: 7.40 },
+    { id: 1, name: '500 Robux', priceUSD: 7.99 },
     { id: 2, name: '1,000 Robux', priceUSD: 14.05 },
     { id: 3, name: '2,000 Robux', priceUSD: 24.85 },
     { id: 4, name: '5,250 Robux', priceUSD: 59.11 }
