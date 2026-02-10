134f247a49ffa1c6ac4c5054823b3a0339c73027
yordinrecarga <yordinrecarga@gmail.com>
2026-02-10 17:59:30 -0400

    Update precios.js

--- precios.js
+++ precios.js
@@ -55,11 +55,11 @@ const arenabreakoutPackagesUSD = [
 
 // Precios base en USD para Blood Strike - NO CAMBIAR ESTOS VALORES
 const bloodstrikePackagesUSD = [
-    { id: 1, name: '105 Gold', priceUSD: 0.75 },
-    { id: 2, name: '320 Gold', priceUSD: 2.19 },
-    { id: 3, name: '540 Gold', priceUSD: 3.58 },
-    { id: 4, name: '1100 Gold', priceUSD: 6.94 },
-    { id: 5, name: '2260 Gold', priceUSD: 13.84 },
+    { id: 1, name: '105 Gold', priceUSD: 0.79 },
+    { id: 2, name: '320 Gold', priceUSD: 3.10 },
+    { id: 3, name: '540 Gold', priceUSD: 5.10 },
+    { id: 4, name: '1100 Gold', priceUSD: 7.80 },
+    { id: 5, name: '2260 Gold', priceUSD: 14.95 },
     { id: 6, name: '5800 Gold', priceUSD: 34.96 },
     { id: 7, name: 'Pase Elite', priceUSD: 3.03 },
     { id: 8, name: 'Pase Elite Plus', priceUSD: 6.83 }
