9510096f211c57f915f8aab58f1abec1f8116963
yordinrecarga <yordinrecarga@gmail.com>
2026-02-10 18:14:49 -0400

    Update precios.js

--- precios.js
+++ precios.js
@@ -57,10 +57,10 @@ const arenabreakoutPackagesUSD = [
 const bloodstrikePackagesUSD = [
     { id: 1, name: '105 Gold', priceUSD: 0.79 },
     { id: 2, name: '320 Gold', priceUSD: 2.45 },
-    { id: 3, name: '540 Gold', priceUSD: 5.10 },
-    { id: 4, name: '1100 Gold', priceUSD: 7.80 },
-    { id: 5, name: '2260 Gold', priceUSD: 14.95 },
-    { id: 6, name: '5800 Gold', priceUSD: 34.96 },
+    { id: 3, name: '540 Gold', priceUSD: 3.69 },
+    { id: 4, name: '1100 Gold', priceUSD: 7.15 },
+    { id: 5, name: '2260 Gold', priceUSD: 14.26 },
+    { id: 6, name: '5800 Gold', priceUSD: 36.01 },
     { id: 7, name: 'Pase Elite', priceUSD: 3.03 },
     { id: 8, name: 'Pase Elite Plus', priceUSD: 6.83 }
 ];
